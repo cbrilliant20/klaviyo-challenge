bd8222a1802ab4b0a77128c57b2b7ea6e804c6b3
Cameron Brilliant <cbrilliant20@gmail.com>
2022-01-07 12:19:40 -0500

    create profile function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const API_KEY = `&APPID=10d288b61704a5239e53beef7ef992cb`
 const UNIT = `&units=imperial`
 const MOUNTAIN = "stratton"
 
+// Requesting latest weather data from OpenWeatherAPI
 function getWeather() {
   fetch(
     `${DOMAIN}q=
@@ -17,4 +18,14 @@ function getWeather() {
     .catch((error) => console.error(error))
 }
 
-getWeather()
+//  Creating Customer profiles with email/SMS opt in.
+function createProfile(e) {
+  let profileData = `{
+    "token": "UNna9N",
+    "properties": {
+      "$email":"${email.value}",
+      "$phone_number:"${phone.value}"
+      "name":"${yourName.value}",
+      "offers":"${offers.value}",
+  }}`
+}
